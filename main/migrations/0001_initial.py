23ba39b16f2f064c09ceed9da7f722638ed15ec8
Anarhan <anarhan.dj@gmail.com>
2021-04-22 18:43:10 +0300

    added new

--- main/migrations/0001_initial.py
+++ main/migrations/0001_initial.py
@@ -31,7 +31,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('text', models.CharField(max_length=100)),
-                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('created_at', models.DateField(auto_now_add=True)),
                 ('is_closed', models.BooleanField(default=False)),
                 ('is_favorite', models.BooleanField(default=False)),
             ],
